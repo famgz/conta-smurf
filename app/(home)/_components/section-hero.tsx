0e7cb5ea6073c7b9566d46439f9d8ed29caf6f19
famgz <famgz@proton.me>
2024-06-26 13:38:33 -0300

    fix: home section alignments

--- app/(home)/_components/section-hero.tsx
+++ app/(home)/_components/section-hero.tsx
@@ -5,13 +5,18 @@ export default function HomeHero() {
   return (
     <div className="flex flex-col gap-5 px-3">
       {/* Hero image */}
-      <div className="relative mt-[-100px] flex-1 overflow-hidden rounded-[40px]">
-        <Image
-          src="/images/home-hero.jpg"
-          alt=""
-          fill
-          className="object-cover object-top"
-        />
+      <div className="mt-[-100px] flex flex-1 items-center justify-start rounded-[40px] bg-[url('/images/home-hero.jpg')] bg-cover bg-top">
+        <div className="hidden space-y-5 pl-28 xl:block">
+          <p className="text-brass text-shadow-sm shadow-brass text-9xl font-extrabold">
+            Be a legend
+          </p>
+          <p className="mx-auto max-w-[500px] text-left font-light">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            magni ipsa ducimus aspernatur recusandae quae, voluptate non
+            necessitatibus illo, id pariatur repellendus velit? Fugiat iure cum
+            tempora quibusdam? Officiis, dicta!
+          </p>
+        </div>
       </div>
 
       {/* Cards */}
--- app/(home)/_components/section-most-wanted.tsx
+++ app/(home)/_components/section-most-wanted.tsx
@@ -2,13 +2,13 @@ import MostWantedCard from '@/app/(home)/_components/most-wanted-card';
 
 export default function MostWanted() {
   return (
-    <div className="flex-center py-12">
-      <div className="h-fit w-full max-w-[1580px] rounded-[30px] bg-black/20 p-10 px-20 pt-6">
-        <h1 className="text-brass text-shadow shadow-brass w-full text-left text-6xl font-extrabold uppercase">
+    <div className="flex-center">
+      <div className="h-fit w-full max-w-[1580px] rounded-[30px] bg-black/20 px-14 pb-14 pt-8">
+        <h1 className="text-brass text-shadow-sm shadow-brass w-full text-left text-6xl font-extrabold uppercase">
           Most Wanted
         </h1>
 
-        <div className="mt-5 flex flex-wrap gap-9">
+        <div className="flex-center mt-5 flex-wrap gap-10">
           <MostWantedCard />
           <MostWantedCard />
           <MostWantedCard />

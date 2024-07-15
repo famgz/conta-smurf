8f9e1196169a305ed9bb32a489eb5e3d4aae0033
famgz <famgz@proton.me>
2024-07-15 12:24:50 -0300

    feat: add restore password page; add initial login and register page responsivity

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -18,7 +18,7 @@ import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { FormEvent } from 'react';
 
-export default function LoginPage() {
+export default function RegisterPage() {
   const form = useForm();
   const router = useRouter();
 
@@ -28,10 +28,12 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="page-section">
-      <div className="grid h-full max-w-[1780px] grid-cols-2 items-center justify-center px-[70px] pb-10 pt-3">
+    <div className="page-section !px-0 max-xl:!pb-5">
+      <div className="flex h-full w-full flex-col items-center justify-center pb-5 max-xl:gap-4 xl:grid xl:max-w-[1780px] xl:grid-cols-2 xl:px-[70px] xl:pb-10 xl:pt-3">
+        <h1 className="text-5xl font-bold xl:hidden">Sign up</h1>
+
         {/* Section image */}
-        <div className="relative aspect-square max-h-full overflow-hidden rounded-3xl border-2 border-[rgba(8,_112,_184,_0.7)] shadow-[2px_10px_30px_rgba(8,_112,_184,_0.7)]">
+        <div className="relative h-[300px] w-full overflow-hidden rounded-3xl border-2 border-[rgba(8,_112,_184,_0.7)] shadow-[2px_10px_30px_rgba(8,_112,_184,_0.7)] max-xl:-mx-5 xl:h-full">
           <Image
             src="/images/register.jpg"
             fill
@@ -40,17 +42,20 @@ export default function LoginPage() {
           />
         </div>
 
-        {/* Login Form */}
-        <div className="flex-center mx-auto w-full max-w-[427px] flex-col">
-          <h1 className="mb-6 text-6xl font-bold">Sign up</h1>
+        {/* Form */}
+        <div className="flex-center mx-auto w-full max-w-[280px] flex-col xl:max-w-[427px]">
+          <h1 className="mb-6 text-6xl font-bold max-xl:hidden">Sign up</h1>
           <Form {...form}>
-            <form onSubmit={handleSubmit} className="w-full text-start">
+            <form
+              onSubmit={handleSubmit}
+              className="w-full space-y-4 text-start"
+            >
               <FormField
                 control={form.control}
                 name="email"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="text-2xl">Email</FormLabel>
+                    <FormLabel className="">Email</FormLabel>
                     <FormControl>
                       <Input
                         type="email"
@@ -68,8 +73,8 @@ export default function LoginPage() {
                 control={form.control}
                 name="password"
                 render={({ field }) => (
-                  <FormItem className="mt-2">
-                    <FormLabel className="text-2xl">Password</FormLabel>
+                  <FormItem className="">
+                    <FormLabel className="">Password</FormLabel>
                     <FormControl>
                       <Input
                         type="password"
@@ -87,8 +92,8 @@ export default function LoginPage() {
                 control={form.control}
                 name="confirm-password"
                 render={({ field }) => (
-                  <FormItem className="mt-2">
-                    <FormLabel className="text-2xl">Confirm Password</FormLabel>
+                  <FormItem className="">
+                    <FormLabel className="">Confirm Password</FormLabel>
                     <FormControl>
                       <Input
                         type="password"
@@ -111,18 +116,18 @@ export default function LoginPage() {
                 </label>
               </div>
 
-              <div className="mt-4 flex justify-between text-xs font-extralight">
+              <div className="mt-4 flex flex-wrap justify-between text-xs font-extralight max-xl:-mx-5">
                 <div className="flex items-center">
                   <Checkbox id="terms2" className="mr-2" />
-                  <span>an uppercase letter</span>
+                  <span className="whitespace-nowrap">an uppercase letter</span>
                 </div>
                 <div className="flex items-center">
                   <Checkbox id="terms2" className="mr-2" />
-                  <span>a number</span>
+                  <span className="whitespace-nowrap">a number</span>
                 </div>
                 <div className="flex items-center">
                   <Checkbox id="terms2" className="mr-2" />
-                  <span>a symbol</span>
+                  <span className="whitespace-nowrap">a symbol</span>
                 </div>
               </div>
 
@@ -149,15 +154,16 @@ export default function LoginPage() {
 
               <Button
                 type="submit"
-                size="xl"
+                size="lg2"
                 variant="golden"
-                className="-ml-4 -mr-4 mt-4 w-full text-4xl"
+                className="w-full text-2xl xl:!mt-10 xl:text-4xl"
               >
-                <span>Create account and login</span>
+                <span>Create account</span>
               </Button>
             </form>
           </Form>
-          <div className="self-start pl-10">
+
+          <div className="self-start xl:pl-10">
             <span className="!m-0 p-0 text-xs font-extralight">
               Already has an account?{' '}
             </span>
--- app/restore-password/page.tsx
+++ app/restore-password/page.tsx
@@ -18,33 +18,37 @@ import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { FormEvent } from 'react';
 
-export default function LoginPage() {
+export default function RestorePasswordPage() {
   const form = useForm();
   const router = useRouter();
 
   function handleSubmit(ev: FormEvent<HTMLFormElement>) {
     ev.preventDefault();
-    router.push('/user');
+    router.push('/login');
   }
 
   return (
     <div className="page-section !px-0 max-xl:!pb-5">
       <div className="flex h-full w-full flex-col items-center justify-center pb-5 max-xl:gap-4 xl:grid xl:max-w-[1780px] xl:grid-cols-2 xl:px-[70px] xl:pb-10 xl:pt-3">
-        <h1 className="text-5xl font-bold xl:hidden">Sign in</h1>
+        <h1 className="whitespace-wrap text-5xl font-bold xl:hidden">
+          Restore Password
+        </h1>
 
         {/* Section image */}
         <div className="relative h-[300px] w-full overflow-hidden rounded-3xl border-2 border-[rgba(8,_112,_184,_0.7)] shadow-[2px_10px_30px_rgba(8,_112,_184,_0.7)] max-xl:-mx-5 xl:h-full">
           <Image
-            src="/images/login.jpg"
+            src="/images/restore-password.jpg"
             fill
-            className="object-cover"
-            alt="PC game cartoon character wearing a garment."
+            className="object-cover object-left"
+            alt="PC game cartoon character"
           />
         </div>
 
         {/* Form */}
         <div className="flex-center mx-auto w-full max-w-[280px] flex-col xl:max-w-[427px]">
-          <h1 className="mb-6 text-6xl font-bold max-xl:hidden">Sign in</h1>
+          <h1 className="mb-6 text-6xl font-bold max-xl:hidden">
+            Restore Password
+          </h1>
           <Form {...form}>
             <form
               onSubmit={handleSubmit}
@@ -58,6 +62,7 @@ export default function LoginPage() {
                     <FormLabel className="">Email</FormLabel>
                     <FormControl>
                       <Input
+                        type="email"
                         placeholder="KDAgragas@email.com"
                         required
                         {...field}
@@ -70,63 +75,48 @@ export default function LoginPage() {
 
               <FormField
                 control={form.control}
-                name="password"
+                name="confirm-email"
                 render={({ field }) => (
-                  <FormItem className="">
-                    <FormLabel className="">Password</FormLabel>
+                  <FormItem>
+                    <FormLabel className="">Confirm Email</FormLabel>
                     <FormControl>
                       <Input
-                        type="password"
-                        placeholder="**********"
+                        type="email"
+                        placeholder="KDAgragas@email.com"
                         required
                         {...field}
                       />
                     </FormControl>
                     <FormMessage />
-                    <Button
-                      type="button"
-                      variant={'link'}
-                      size={'sm'}
-                      className="!m-0 p-0 text-xs font-extralight"
-                      asChild
-                    >
-                      <Link href="restore-password">Forget your password?</Link>
-                    </Button>
                   </FormItem>
                 )}
               />
-
-              <div className="mt-10 flex items-center xl:mt-20">
-                <Checkbox id="terms2" className="mr-2" />
-                <label
-                  htmlFor="terms2"
-                  className="text-xs font-extralight leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                  Remember me
-                </label>
-              </div>
 
               <Button
                 type="submit"
                 size="xl"
                 variant="golden"
-                className="mt-4 w-full text-4xl xl:text-6xl"
+                className="!xl:mt-20 !mt-10 w-full text-2xl xl:text-4xl"
               >
-                <span>Login</span>
+                <span>Send Email</span>
               </Button>
             </form>
           </Form>
 
-          <p className="my-4 text-center font-light xl:text-3xl">OR</p>
-
-          <Button
-            type="button"
-            size="lg2"
-            variant="white"
-            className="w-full text-xl xl:mt-4 xl:text-4xl"
-          >
-            <Link href={'/register'}>Create Account</Link>
-          </Button>
+          <div className="self-start xl:pl-10">
+            <span className="!m-0 p-0 font-extralight max-xl:text-xs">
+              You remembered the password?{' '}
+            </span>
+            <Button
+              type="button"
+              variant={'link'}
+              size={'sm'}
+              className="!m-0 p-0 text-xs font-extralight"
+              asChild
+            >
+              <Link href={'/login'}>try login!</Link>
+            </Button>
+          </div>
         </div>
       </div>
     </div>

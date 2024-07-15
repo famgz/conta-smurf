711e37b30c656988487fd8975d6df777b4002f08
famgz <famgz@proton.me>
2024-07-14 23:28:22 -0300

    feat: add elojob page be agent section responsivity

--- app/elojob/_components/section-be-agent.tsx
+++ app/elojob/_components/section-be-agent.tsx
@@ -10,7 +10,6 @@ import {
   FormMessage,
 } from '@/app/_components/ui/form';
 import { Input } from '@/app/_components/ui/input';
-import { Textarea } from '@/app/_components/ui/textarea';
 import Image from 'next/image';
 import { useForm } from 'react-hook-form';
 
@@ -18,25 +17,22 @@ export default function EloJobBeAgentSection() {
   const form = useForm();
 
   return (
-    <div className="grid h-full max-w-[1780px] grid-cols-[3fr_2fr] items-center justify-between gap-5 !overflow-visible px-40 pb-14 pt-4">
+    <div className="grid h-full max-w-[1780px] items-center justify-center gap-5 !overflow-visible md:grid-cols-2 md:justify-between xl:px-40">
       {/* Side info */}
-      <div className="hide-scrollbar mx-auto flex h-full w-full max-w-[573px] flex-col space-y-6 overflow-y-auto rounded-[30px] bg-folder px-20 py-8">
-        <h2 className="text-5xl font-bold">Be an agent</h2>
+      <div className="hide-scrollbar mx-auto flex h-full w-full max-w-[573px] flex-col gap-3 overflow-y-auto rounded-[30px] bg-folder px-10 py-6 xl:gap-6 xl:px-20 xl:py-8">
+        <h2 className="text-3xl font-bold xl:text-5xl">Be an agent</h2>
 
         <Form {...form}>
           <form
             onSubmit={() => {}}
-            className="h-full w-full space-y-5 text-start"
+            className="flex h-full w-full flex-1 flex-col gap-4 text-start"
           >
             <FormField
               name="user"
               render={({ field }) => (
                 <FormItem className="w-full">
-                  <FormLabel className="text-2xl">Your Name or Nick</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Gragasthegreat" maxLength={20} />
-                  </FormControl>
-                  <FormMessage />
+                  <FormLabel className="">Your Name or Nick</FormLabel>
+                  <Input placeholder="Gragasthegreat" maxLength={20} />
                 </FormItem>
               )}
             />
@@ -45,15 +41,12 @@ export default function EloJobBeAgentSection() {
               name="email"
               render={({ field }) => (
                 <FormItem className="w-full">
-                  <FormLabel className="text-2xl">Email for contact</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Gragasthegreat@Email.com"
-                      maxLength={20}
-                      type="email"
-                    />
-                  </FormControl>
-                  <FormMessage />
+                  <FormLabel className="">Email for contact</FormLabel>
+                  <Input
+                    placeholder="Gragasthegreat@Email.com"
+                    maxLength={20}
+                    type="email"
+                  />
                 </FormItem>
               )}
             />
@@ -61,14 +54,14 @@ export default function EloJobBeAgentSection() {
             <FormField
               name="reason"
               render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormLabel className="text-2xl">
+                <FormItem className="flex flex-1 flex-col gap-2">
+                  <FormLabel className="">
                     Why do you want to be an agent?
                   </FormLabel>
-                  <FormControl>
-                    <Textarea placeholder="" />
-                  </FormControl>
-                  <FormMessage />
+                  <textarea
+                    className="w-full flex-1 rounded-md p-2 text-sm text-muted xl:text-lg"
+                    placeholder="Talk about your ranks, playing time, favorite roles and characters"
+                  />
                 </FormItem>
               )}
             />
@@ -76,12 +69,15 @@ export default function EloJobBeAgentSection() {
             <FormField
               name="gamin-story"
               render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormLabel className="text-2xl">
+                <FormItem className="flex flex-1 flex-col gap-2">
+                  <FormLabel className="">
                     Tell us your gaming story (League of Legends or Valorant)
                   </FormLabel>
                   <FormControl>
-                    <Textarea placeholder="Talk about your ranks, playing time, favorite roles and characters" />
+                    <textarea
+                      className="w-full flex-1 rounded-md p-2 text-sm text-muted xl:text-lg"
+                      placeholder="Talk about your ranks, playing time, favorite roles and characters"
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -89,7 +85,7 @@ export default function EloJobBeAgentSection() {
             />
 
             <Button
-              className="active-side-menu-button h-[54px] w-full text-3xl"
+              className="active-side-menu-button h-[54px] w-full text-3xl hover:shadow-[0px_0px_20px_0px_rgba(0,_255,_224,_0.49)]"
               variant={'secondary'}
             >
               Send
@@ -99,7 +95,7 @@ export default function EloJobBeAgentSection() {
       </div>
 
       {/* Section image */}
-      <div className="z-1 relative h-full w-full overflow-hidden rounded-3xl border-2 border-[rgba(8,_112,_184,_0.7)] shadow-[0px_0px_50px_0px_rgba(0,_255,_224,_0.49)]">
+      <div className="z-1 relative h-full w-full overflow-hidden rounded-3xl border-2 border-[rgba(8,_112,_184,_0.7)] shadow-[0px_0px_50px_0px_rgba(0,_255,_224,_0.49)] max-md:hidden">
         <Image
           src="/images/be-agent.jpg"
           fill
@@ -110,4 +106,3 @@ export default function EloJobBeAgentSection() {
     </div>
   );
 }
-``;

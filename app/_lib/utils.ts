82acb2ac2b03f9013c76d38e1e584278b3181c65
famgz <famgz@proton.me>
2024-09-07 12:08:31 -0300

    feat: add format currency utility

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -7,4 +8,13 @@ export function cn(...inputs: ClassValue[]) {
 
 export function plainify<T>(obj: T): T {
   return JSON.parse(JSON.stringify(obj));
+}
+
+export function formatPrice(price: Prisma.Decimal | number) {
+  price = Number(price);
+  return Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  }).format(Number(price));
 }
